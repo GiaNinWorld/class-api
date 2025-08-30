fbd775d797f1d1454d19e9dd88aebaf3166aaded
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2025-08-30 18:53:43 -0300

    Alterada rota "raiz" do ProductController

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -2,13 +2,13 @@ import { Controller, Get, Param } from '@nestjs/common'
 import { Product } from '../model/Product'
 import { ProductService } from '../service/ProductService'
 
-@Controller()
+@Controller('product')
 export class ProductController {
   constructor(
     private readonly productService: ProductService,
   ) {}
 
-  @Get('product/:id')
+  @Get(':id')
   getProductById(@Param('id') id: string): Product {
     return this.productService.findProductById(id)
   }
